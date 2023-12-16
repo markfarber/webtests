4f5f116e4e45fa5b1bfaf7ae7f629f72c0c73fba
Shalty <shealtiel84@gmail.com>
2023-12-17 01:28:45 +0200

    login

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
+    authDomain: "tyg-stage-b8e16.firebaseapp.com",
+    projectId: "tyg-stage-b8e16",
+    storageBucket: "tyg-stage-b8e16.appspot.com",
+    messagingSenderId: "678914468365",
+    appId: "1:678914468365:web:0345776b1da9cdd48bc344",
+    measurementId: "G-FDJ0SB6C4G",
+  };
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  firebase.auth().useDeviceLanguage();
+  
+
+
+
+function getUserFromFS(user){
+  console.log(user)
+  var docRef = db.collection("users").doc(user.uid);
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
