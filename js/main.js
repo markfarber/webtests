082709906ac890e1e1aafb8b45438ecfa36150df
Shalty <shealtiel84@gmail.com>
2023-12-17 02:44:52 +0200

    login

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 
 
   window.onload = ()=>{
-    const firebaseConfig = {
-      apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
-      authDomain: "tyg-stage-b8e16.firebaseapp.com",
-      projectId: "tyg-stage-b8e16",
-      storageBucket: "tyg-stage-b8e16.appspot.com",
-      messagingSenderId: "678914468365",
-      appId: "1:678914468365:web:0345776b1da9cdd48bc344",
-      measurementId: "G-FDJ0SB6C4G",
-    };
-    // Initialize Firebase
-    const app = firebase.initializeApp(firebaseConfig);
-    firebase.auth().useDeviceLanguage();
+   
     
   }
 
-
+  const firebaseConfig = {
+    apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
+    authDomain: "tyg-stage-b8e16.firebaseapp.com",
+    projectId: "tyg-stage-b8e16",
+    storageBucket: "tyg-stage-b8e16.appspot.com",
+    messagingSenderId: "678914468365",
+    appId: "1:678914468365:web:0345776b1da9cdd48bc344",
+    measurementId: "G-FDJ0SB6C4G",
+  };
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  
+  
 function getUserFromFS(user){
   console.log(user)
   var docRef = db.collection("users").doc(user.uid);
