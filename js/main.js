f1cf8cf52d6fb05970ae27269c5d369d73c00de9
Mark Farber <mark@farberz.info>
2023-12-26 23:18:28 +0200

    messaging WIP

--- js/main.js
+++ js/main.js
@@ -1,32 +1,54 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
-    authDomain: "tyg-stage-b8e16.firebaseapp.com",
-    projectId: "tyg-stage-b8e16",
-    storageBucket: "tyg-stage-b8e16.appspot.com",
-    messagingSenderId: "678914468365",
-    appId: "1:678914468365:web:0345776b1da9cdd48bc344",
-    measurementId: "G-FDJ0SB6C4G",
-  };
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
+  authDomain: "tyg-stage-b8e16.firebaseapp.com",
+  projectId: "tyg-stage-b8e16",
+  storageBucket: "tyg-stage-b8e16.appspot.com",
+  messagingSenderId: "678914468365",
+  appId: "1:678914468365:web:0345776b1da9cdd48bc344",
+  measurementId: "G-FDJ0SB6C4G",
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-function getUserFromFS(user){
-  console.log(user)
+function getUserFromFS(user) {
+  console.log(user);
   const db = firebase.firestore();
   var docRef = db.collection("users").doc(user.uid);
 
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-            localStorage.setItem("UID", user.uid);
-            window.location = "homePage.html"
-
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        localStorage.setItem("UID", user.uid);
+        window.location = "homePage.html";
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
     });
+}
+
+function init_messaging() {
+    messaging.getToken((token) => {
+        console.log("token = " + token)
+    })
+    
+//   Notification.requestPermission()
+//     .then((permission) => {
+//       if (permission === "granted") {
+//         console.log("granted");
+//         return messaging.getToken();
+//       }
+//     })
+//     .then((token) => {
+//       console.log("token = " + token);
+//     })
+//     .catch((e) => {
+//       console.log("got error " + e);
+//     });
 }
