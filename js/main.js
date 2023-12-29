3c1939ae6ce1c3cc5e58beb9ffc311c2a907b558
Shalty <shealtiel84@gmail.com>
2023-12-29 16:09:12 +0200

    service

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+
 const firebaseConfig = {
   apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
   authDomain: "tyg-stage-b8e16.firebaseapp.com",
+  databaseURL: "https://tyg-stage-b8e16-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "tyg-stage-b8e16",
   storageBucket: "tyg-stage-b8e16.appspot.com",
   messagingSenderId: "678914468365",
   appId: "1:678914468365:web:0345776b1da9cdd48bc344",
-  measurementId: "G-FDJ0SB6C4G",
+  measurementId: "G-FDJ0SB6C4G"
 };
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
-
+// const functions = firebase.functions(app,'europe-west2');
 
 messaging.onMessage(messaging, (payload) => {
   console.log('Message received. ', payload);
@@ -30,12 +32,23 @@ Notification.requestPermission().then((permission) => {
       //sendTokenToServer(currentToken);
       //updateUIForPushEnabled(currentToken);
 
-      firebase.app().functions('europe-west2').httpsCallable('user_update_token')().then((result) => {
-        // Read result of the Cloud Function.
-        var sanitizedMessage = result.data.text;
-        console.log(result)
-      });
-      // var user_update_token = firebase.app().functions('europe-west2').httpsCallable('user_update_token');
+      // firebase.app().functions('europe-west2').httpsCallable('user_update_token')().then((result) => {
+      //   // Read result of the Cloud Function.
+      //   var sanitizedMessage = result.data.text;
+      //   console.log(result)
+      // });
+
+
+      var user_update_token = firebase.functions(app,'europe-west2').httpsCallable('user_update_token');
+      user_update_token({ text: messageText })
+          .then((result) => {
+            // Read result of the Cloud Function.
+            var sanitizedMessage = result.data.text;
+          });
+
+
+
+      // var user_update_token = messaging.functions('europe-west2').httpsCallable('user_update_token');
       // user_update_token()
       //   .then((result) => {
       //     // Read result of the Cloud Function.
@@ -69,27 +82,27 @@ Notification.requestPermission().then((permission) => {
 
 
 
-function getUserFromFS(user) {
-  console.log(user);
-  const db = firebase.firestore();
-  var docRef = db.collection("users").doc(user.uid);
+// function getUserFromFS(user) {
+//   console.log(user);
+//   const db = firebase.firestore();
+//   var docRef = db.collection("users").doc(user.uid);
 
-  docRef
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-        localStorage.setItem("UID", user.uid);
-        window.location = "homePage.html";
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    })
-    .catch((error) => {
-      console.log("Error getting document:", error);
-    });
-}
+//   docRef
+//     .get()
+//     .then((doc) => {
+//       if (doc.exists) {
+//         console.log("Document data:", doc.data());
+//         localStorage.setItem("UID", user.uid);
+//         window.location = "homePage.html";
+//       } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//       }
+//     })
+//     .catch((error) => {
+//       console.log("Error getting document:", error);
+//     });
+// }
 
 
 function init_messaging() {
