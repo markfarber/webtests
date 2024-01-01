bb721dd95032d677b21686789df9a42fb3861312
Mark Farber <mark@farberz.info>
2024-01-01 13:30:27 +0200

    login_test

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ const functions = app.functions('europe-west2');
 function init_messaging(nav){
   messaging = firebase.messaging();
   if ('serviceWorker' in nav){
+    console.log("here 1")
     nav.serviceWorker.register('firebase-messaging-sw.js')
     .then((registration) => {
       console.log(registration)
