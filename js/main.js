0d2a5a535fda98f0326c3d6cfa248d335ea46951
Mark Farber <mark@farberz.info>
2024-01-01 16:25:12 +0200

    messaging WIP

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-const USER_KEY = "user"
+const USER_KEY = "user";
 const firebaseConfig = {
   apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
   authDomain: "tyg-stage-b8e16.firebaseapp.com",
-  databaseURL: "https://tyg-stage-b8e16-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://tyg-stage-b8e16-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "tyg-stage-b8e16",
   storageBucket: "tyg-stage-b8e16.appspot.com",
   messagingSenderId: "678914468365",
   appId: "1:678914468365:web:0345776b1da9cdd48bc344",
-  measurementId: "G-FDJ0SB6C4G"
+  measurementId: "G-FDJ0SB6C4G",
 };
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const functions = app.functions('europe-west2');
-messaging = firebase.messaging();
+const functions = app.functions("europe-west2");
 
+class Messaging {
+  messaging = firebase.messaging();
 
-
-function init_messaging(nav){
-  console.log('init_messaging called with ' + JSON.stringify(nav))
-  // messaging = firebase.messaging();
-  if ('serviceWorker' in nav){
-    nav.serviceWorker.register('firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log(registration)
-      functions.httpsCallable("user_update_token")()
-      .then((result) => {console.log('Token update: ' + result)})
-    })
-    .catch((e) => {console.error('init_messaging error ' + e)})
+  init_messaging(nav) {
+    console.log("init_messaging called with " + JSON.stringify(nav));
+    // messaging = firebase.messaging();
+    if ("serviceWorker" in nav) {
+      nav.serviceWorker
+        .register("firebase-messaging-sw.js")
+        .then((registration) => {
+          console.log(registration);
+          functions
+            .httpsCallable("user_update_token")()
+            .then((result) => {
+              console.log("Token update: " + result);
+            });
+        })
+        .catch((e) => {
+          console.error("init_messaging error " + e);
+        });
+    }
   }
 }
 
@@ -41,7 +49,6 @@ function init_messaging(nav){
     });
   }*/
 
-
 //push messeg
 /*messaging.onMessage(messaging, (payload) => {
   console.log('Message received. ', payload);
@@ -51,33 +58,26 @@ function init_messaging(nav){
 
 // Notification request Permission
 Notification.requestPermission().then((permission) => {
-  if (permission === 'granted') {
-    console.log('Notification permission granted.');
-  }});
-
-  
+  if (permission === "granted") {
+    console.log("Notification permission granted.");
+  }
+});
 
 // function init_messaging() {
 //   vapidKey = "BJVCp-sxo-XLCPW1xeDTCsYxKG9JRtNf70vgD4IK7DNM6byehbvwbYHp-n-tf-Z2DKobh0KNoboUiQCpslfmkNQ"
-  // messaging.getToken(messaging, { vapidKey }).then((currentToken) => {
-  //   if (currentToken) {
-      
-    
+// messaging.getToken(messaging, { vapidKey }).then((currentToken) => {
+//   if (currentToken) {
 
-      //console.log(currentToken)
-  //   } else {
-  //     // Show permission request.
-  //     console.log('No registration token available. Request permission to generate one.');
-  //     // Show permission UI.
-  //     //updateUIForPushPermissionRequired();
-  //     //setTokenSentToServer(false);
-  //   }
-  // }).catch((err) => {
-  //   console.log('An error occurred while retrieving token. ', err);
-  // });
+//console.log(currentToken)
+//   } else {
+//     // Show permission request.
+//     console.log('No registration token available. Request permission to generate one.');
+//     // Show permission UI.
+//     //updateUIForPushPermissionRequired();
+//     //setTokenSentToServer(false);
+//   }
+// }).catch((err) => {
+//   console.log('An error occurred while retrieving token. ', err);
+// });
 
-
-  
 //}
-
-
