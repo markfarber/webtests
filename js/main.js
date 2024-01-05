a0a61e4da4f75ea1b47e7050024453f94bff742f
Shalty <shealtiel84@gmail.com>
2024-01-05 02:51:46 +0200

    sw

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ if('Notification' in window){
 
 
           functions
-          .httpsCallable("user_update_token")(params = {device_token:currentToken})
+          .httpsCallable("user_update_token")(JSON.stringify({device_token:currentToken}))
           .then((user_obj) => {
             console.log(user_obj);
             // console.log(result)
