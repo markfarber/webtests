a13a762d0d913cbc182f046abcb495a084850138
Shalty <shealtiel84@gmail.com>
2024-01-05 22:56:40 +0200

    uc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const USER_KEY = "user";
+const M_P_KEY = "BJVCp-sxo-XLCPW1xeDTCsYxKG9JRtNf70vgD4IK7DNM6byehbvwbYHp-n-tf-Z2DKobh0KNoboUiQCpslfmkNQ"
 const firebaseConfig = {
   apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
   authDomain: "tyg-stage-b8e16.firebaseapp.com",
@@ -31,39 +32,29 @@ if('Notification' in window){
     }
   })
 }
-
+corentUser = {}
 firebase.auth().onAuthStateChanged((user) => {
+  
+  
   if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/v8/firebase.User
-    var uid = user.uid;
-    console.log("onAuthStateChanged uid = "+uid)
+   
+    corentUser = localStorage.getItem(USER_KEY)
     messagingSendToken()
   } else {
-    // User is signed out
-    // ...
+    window.location.href = "/index.html"
+
   }
 });
 
 function messagingSendToken(){
-  if(window.location.href == "https://dev.blaster.co.il/homePage.html"){
-    messaging.getToken("BJVCp-sxo-XLCPW1xeDTCsYxKG9JRtNf70vgD4IK7DNM6byehbvwbYHp-n-tf-Z2DKobh0KNoboUiQCpslfmkNQ" )
+  if(window.location.href.indexOf("/homePage.html")){
+    messaging.getToken(M_P_KEY)
     .then((currentToken)=>{
       if(currentToken){
-            console.log(currentToken) 
 
-            const data = {device_token:currentToken}
-            functions.httpsCallable("user_update_token")(data)
-            .then((user_obj) => {
-              user_obj.data.token = currentToken
-              console.log(user_obj);
-              // console.log(result)
-              localStorage.setItem(USER_KEY, JSON.stringify(user_obj.data));
-              //window.location.href = "homePage.html";
-                              })
-            .catch((e) => {
-              console.error("error " + e);
-            });
+            functions.httpsCallable("user_update_token")({device_token:currentToken})
+            .then((user_obj) => localStorage.setItem(USER_KEY, JSON.stringify(corentUser['token'] = currentToken)))
+            .catch((e) => console.error("error " + e)); 
       }else{
           console,log("token error = "+errr)
       }
