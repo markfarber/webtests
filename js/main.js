c745d10f3359fe5e8010ddd0c7b2375e62fd22d2
Mark Farber <mark@farberz.info>
2024-01-07 01:43:39 +0200

    messaging WIP

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 const USER_KEY = "user";
-const M_P_KEY = "BJVCp-sxo-XLCPW1xeDTCsYxKG9JRtNf70vgD4IK7DNM6byehbvwbYHp-n-tf-Z2DKobh0KNoboUiQCpslfmkNQ"
+const M_P_KEY = "BJVCp-sxo-XLCPW1xeDTCsYxKG9JRtNf70vgD4IK7DNM6byehbvwbYHp-n-tf-Z2DKobh0KNoboUiQCpslfmkNQ";
 const firebaseConfig = {
   apiKey: "AIzaSyAe2AQgp3soMCBHX5uCtDuYRDdFAwondVU",
   authDomain: "tyg-stage-b8e16.firebaseapp.com",
@@ -17,71 +16,61 @@ const functions = app.functions("europe-west2");
 const messaging = app.messaging("europe-west2");
 const storage = app.storage();
 
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register("firebase-messaging-sw.js")
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("firebase-messaging-sw.js");
 }
 
-if('Notification' in window){
-  Notification.requestPermission().then((premition)=>{
-    if(premition == 'denied')
-    {
-      console.log("the user denied the premition")
-      return
+if ("Notification" in window) {
+  Notification.requestPermission().then((premition) => {
+    if (premition == "denied") {
+      console.log("the user denied the premition");
+      return;
     }
-    if(premition == 'granted'){
-      console.log("premition granted")
+    if (premition == "granted") {
+      console.log("premition granted");
     }
-  })
+  });
 }
-corentUser = {}
+corentUser = {};
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-
-      corentUser = JSON.parse(localStorage.getItem(USER_KEY))
-      messagingSendToken()
-      showCardOnUI() 
-    
+    corentUser = JSON.parse(localStorage.getItem(USER_KEY));
+    messagingSendToken();
+    showCardOnUI();
   } else {
-    if(!window.location.href.indexOf("/index.html"))
-        window.location.href = "/index.html"
-
+    if (!window.location.href.indexOf("/index.html")) window.location.href = "/index.html";
   }
 });
 
-function messagingSendToken(){
-  if(window.location.href.indexOf("/homePage.html")){
-    messaging.getToken({vapidKey: M_P_KEY})
-    .then((currentToken)=>{
-      console.log('token = ' + currentToken)
-      if(currentToken){
-            data = {device_token:currentToken}
-            functions.httpsCallable("user_update_token")(data)
-            .then((result) =>{
-              corentUser.data['token'] = currentToken
-              localStorage.setItem(USER_KEY, JSON.stringify(corentUser))
-              console.log('user_update_token yield -> ' + result.data)
+function messagingSendToken() {
+  if (window.location.href.indexOf("/homePage.html")) {
+    messaging
+      .getToken({ vapidKey: M_P_KEY })
+      .then((currentToken) => {
+        console.log("token = " + currentToken);
+        if (currentToken) {
+          data = { device_token: currentToken };
+          functions
+            .httpsCallable("user_update_token")(data)
+            .then((result) => {
+              corentUser.data["token"] = currentToken;
+              localStorage.setItem(USER_KEY, JSON.stringify(corentUser));
+              console.log("user_update_token yield -> " + result.data);
             })
-            .catch((e) => console.error("error " + e)); 
-      }else{
-          console,log("token error = "+errr)
-      }
-    })
-    .catch((err)=>{
-      console.log(err)
-    });
+            .catch((e) => console.error("error " + e));
+        } else {
+          console, log("token error = " + errr);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
 messaging.onMessage((payload) => {
-  console.log("message received -> " + JSON.stringify(payload))
-  navigator.serviceWorker.ready()
-  .then((registration) => {
-    registration.showNotification(payload.notification.title, payload.notification.body)
-  })
-})
-
-
-
-
-
-
+  console.log("message received -> " + JSON.stringify(payload));
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.showNotification(payload.notification.title, payload.notification.body);
+  });
+});
