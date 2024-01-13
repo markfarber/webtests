2f439adf767c1df8f8ad6fe2e3ebdfd238c132a8
Shalty <shealtiel84@gmail.com>
2024-01-14 01:47:03 +0200

    ui

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,7 +30,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = 'מסר חדש מ-שי';
   const notificationOptions = {
     body: 'Background Message body.',
-    icon: 'https://firebasestorage.googleapis.com/v0/b/tyg-stage-b8e16.appspot.com/o/800%2Flogo.png?alt=media&token=5424e1b5-5a2d-462b-82ff-40b18747603e',
+    // icon: 'https://firebasestorage.googleapis.com/v0/b/tyg-stage-b8e16.appspot.com/o/800%2Flogo.png?alt=media&token=5424e1b5-5a2d-462b-82ff-40b18747603e',
     renotify:true,
     image :"",
     dir :"rtl",
