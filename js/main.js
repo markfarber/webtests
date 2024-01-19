3c541479552ee06d623ad5dd104a5df190fdf1cb
Shalty <shealtiel84@gmail.com>
2024-01-19 03:56:19 +0200

    ui

--- js/main.js
+++ js/main.js
@@ -103,9 +103,9 @@ function messagingSendToken() {
 
 messaging.onMessage((payload) => {
   console.log("message received -> " + JSON.stringify(payload));
-  navigator.serviceWorker.ready.then((registration) => {
-    registration.showNotification(payload.notification.title);
-  });
+  // navigator.serviceWorker.ready.then((registration) => {
+  //   registration.showNotification(payload.notification.title);
+  // });
 });
 
 if(window.location.href.indexOf("/index.html")<0)
