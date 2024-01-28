9d7aee9326771f552a288a9f4e632ceb763b2363
Shalty <shealtiel84@gmail.com>
2024-01-28 16:17:52 +0200

    ui

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -59,6 +59,11 @@ messaging.onBackgroundMessage((payload) => {
     icon: 'https://storage.googleapis.com/tyg-stage-b8e16.appspot.com/static/logo.png',
     badge: 'https://storage.googleapis.com/tyg-stage-b8e16.appspot.com/static/logo.png',
     image:  payload.data.image,
+    data: {
+      click_action: clickAction,
+      notificationType: payload.data.type,
+      // Add any additional custom data you need
+    },
   };
 
 
